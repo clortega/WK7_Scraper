f84199fa2ddee8c214b6eb6d74f8da04988e6340
clortega <carlos.l.ortega@outlook.com>
2023-12-06 15:59:57 -0800

    Last commit

--- Scraper.java
+++ Scraper.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.HashSet;
 import java.util.regex.Pattern;
 
 public class Scraper extends JFrame {
@@ -15,8 +16,11 @@ public class Scraper extends JFrame {
     JTextField urlTextField; 
     DefaultTableModel tableModel;
     JTable jTable;
-    JTextField regexTextField;
+    JComboBox<String> regexComboBox;
     JButton btn;
+    JButton resetbtn;
+    HashSet<String> matches = new HashSet<String>();
+
     
     public Scraper() {
         super("Scrape the application");
@@ -43,14 +47,23 @@ public class Scraper extends JFrame {
 
         JPanel southJPanel = new JPanel();
 
-        regexTextField = new JTextField("Enter a REGEX", 20);
-        southJPanel.add(regexTextField);
+        regexComboBox = new JComboBox<>();
+        regexComboBox.addItem("\\d{3}-\\d{3}\\-\\d{4}");
+        regexComboBox.addItem("[0-9]");
+        regexComboBox.addItem("[A-Za-z0-9\\.]+\\@[A-Za-z0-9]+\\.[A-Za-z0-9]+");
+
+        // regexTextField = new JTextField("Enter a REGEX", 20);
+
+        southJPanel.add(regexComboBox);
 
         btn = new JButton("Click me!");
         btn.addActionListener(this::SearchPage);
         // btn.addActionListener(e -> SearchPage(e)); //this is works in place of the above line
-
+    
+        resetbtn = new JButton("Reset");
+        resetbtn.addActionListener(this::Reset);
         southJPanel.add(btn);
+        southJPanel.add(resetbtn);
 
         add(southJPanel, BorderLayout.SOUTH);
 
@@ -61,7 +74,12 @@ public class Scraper extends JFrame {
         
     }
 
+    public void Reset(ActionEvent e) {
+        tableModel.setRowCount(0);
+        matches.clear();
+    }
     public void SearchPage(ActionEvent e) {
+        //tableModel.setRowCount(0);
         try{  //wrapped it in a try catch, because it was not handling an exception.
             URL url = new URL(urlTextField.getText()); //the constructor we are using, 'URL', is deprecated and subject to removal in the future..
             URLConnection urlConnection = url.openConnection();
@@ -70,14 +88,22 @@ public class Scraper extends JFrame {
             BufferedReader bufferedReader = new BufferedReader(streamReader);
             String line = null;
 
+
+
             while((line = bufferedReader.readLine()) != null) { //while we have lines to read... 
                 // regex pattern matching
-                Pattern pattern = Pattern.compile(regexTextField.getText());
+                Pattern pattern = Pattern.compile(regexComboBox.getSelectedItem().toString());
                 java.util.regex.Matcher match = pattern.matcher(line);
 
                 // add to our table
                 if(match.find()){
+                    if(matches.contains(match.group())){
+                    //do nothing
+                } else {
+                    var grouping = match.group();
+                    matches.add(match.group());
                     tableModel.addRow(new Object[]{String.valueOf(tableModel.getRowCount() + 1), match.group()});
+                }
                 }
 
             }
@@ -89,5 +115,7 @@ public class Scraper extends JFrame {
     }
 
 }
-
 //   \d{3}-\d{3}\-\d{4}
+
+
+/// [A-Za-z0-9\.]+\@[A-Za-z0-9]+\.[A-Za-z0-9]+
